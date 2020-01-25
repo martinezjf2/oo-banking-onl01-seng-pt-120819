00ee6f6c7a1f324dec28b4b9ae501494896e9dc8
Jeffrey Martinez <martinezjf2@gmail.com>
2020-01-25 16:32:42 +0000

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,33 @@
 class BankAccount
+attr_reader :name
+attr_accessor :balance, :status
+
+  def initialize(name)
+  @name = name
+  @balance = 1000
+  @status = 'open'
+  end
+
+  def deposit(deposit_amount)
+  @balance += deposit_amount
+  end
+
+  def display_balance
+    p "Your balance is $#{self.balance}." #difference between p, prints, puts and when to use them
+  end
+
+  def valid?
+    @balance > 0 && @status == 'open' if true
+  end
+
+  def close_account
+    @status = 'closed'
+  end
+
+
+
+
+
+
 
 end
--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,29 @@
 class Transfer
   # your code here
+  attr_accessor :sender, :receiver, :status, :amount, :bankaccount
+
+  def initialize(sender, reciever, amount)
+    @sender = sender
+    @receiver = reciever
+    @status = 'pending'
+    @amount = amount
+  end
+
+  def valid?
+
+  end
+
+  def execute_transaction
+
+  end
+
+  def reverse_transfer
+
+  end
+
+  
+
+
+
+
 end
